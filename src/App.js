317b9ad774b229fdd958466c909f1b1fd1aba8f5
jaguso <jagutzo@gmail.com>
2018-09-28 19:47:00 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import Courses from './Courses';
+import './App.css';
+
+const client = new ApolloClient({
+  uri: "https://vm8mjvrnv3.lp.gql.zone/graphql"
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <Courses />
+    </div>
+  </ApolloProvider>
+);
+
+export default App;
